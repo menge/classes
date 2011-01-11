df795091f8d6d1e7508e6d7a70f3c55c051c36d7
Emil Meng <emil.meng@gmail.com>
2011-01-11 14:50:00 -0700

    removed all implementation except for printing

--- DataStructures/HW01/Account.cxx
+++ DataStructures/HW01/Account.cxx
@@ -4,73 +4,33 @@
 
 namespace net_oatsnet_class_datastructures {
    Account::Account() {
-      name = "";
-      checkingsAmount = 0;
-      savingsAmount = 0;
    }
 
    Account::Account(string name, unsigned long checkingsAmount, unsigned long savingsAmount) {
-      this->name = name;
-      this->checkingsAmount = checkingsAmount;
-      this->savingsAmount = savingsAmount;
    }
 
    Account::~Account() {
    }
 
    void Account::deposit(int accountType, unsigned long amount) {
-      switch(accountType) {
-      case Account::CHECKINGS:
-         {
-            checkingsAmount += amount;
-            break;
-         }
-      case Account::SAVINGS:
-         {
-            savingsAmount += amount;
-            break;
-         }
-      default:
-         {
-            assert(false);
-            break;
-         }
-      }
    }
 
    void Account::withdraw(int accountType, unsigned long amount) {
-      switch(accountType) {
-      case Account::CHECKINGS:
-         {
-            checkingsAmount -= amount;
-            break;
-         }
-      case Account::SAVINGS:
-         {
-            savingsAmount -= amount;
-            break;
-         }
-      default:
-         {
-            assert(false);
-            break;
-         }
-      }
    }
 
    unsigned long long Account::getTotalAccountValue() {
-      return (unsigned long long) checkingsAmount + (unsigned long long) savingsAmount;
+      return 0;
    }
 
    unsigned long Account::getCheckingsAmount() {
-      return checkingsAmount;
+      return 0;
    }
 
    unsigned long Account::getSavingsAmount() {
-      return savingsAmount;
+      return 0;
    }
 
    string Account::getName() {
-      return name;
+      return string("");
    }
 }
