bc181ebe795acf500191251fa852c2dab97a2be5
Emil Meng <emil.meng@gmail.com>
2011-01-11 21:52:48 -0700

    due to merging, instructor's answers were wiped... now restored with the correct answers

--- DataStructures/HW02/Account.cxx
+++ DataStructures/HW02/Account.cxx
@@ -4,33 +4,73 @@
 
 namespace net_oatsnet_class_datastructures {
    Account::Account() {
+      name = "";
+      checkingsAmount = 0;
+      savingsAmount = 0;
    }
 
    Account::Account(string name, unsigned long checkingsAmount, unsigned long savingsAmount) {
+      this->name = name;
+      this->checkingsAmount = checkingsAmount;
+      this->savingsAmount = savingsAmount;
    }
 
    Account::~Account() {
    }
 
    void Account::deposit(int accountType, unsigned long amount) {
+      switch(accountType) {
+      case Account::CHECKINGS:
+         {
+            checkingsAmount += amount;
+            break;
+         }
+      case Account::SAVINGS:
+         {
+            savingsAmount += amount;
+            break;
+         }
+      default:
+         {
+            assert(false);
+            break;
+         }
+      }
    }
 
    void Account::withdraw(int accountType, unsigned long amount) {
+      switch(accountType) {
+      case Account::CHECKINGS:
+         {
+            checkingsAmount -= amount;
+            break;
+         }
+      case Account::SAVINGS:
+         {
+            savingsAmount -= amount;
+            break;
+         }
+      default:
+         {
+            assert(false);
+            break;
+         }
+      }
    }
 
    unsigned long long Account::getTotalAccountValue() {
-      return 0;
+      return (unsigned long long) checkingsAmount + (unsigned long long) savingsAmount;
    }
 
    unsigned long Account::getCheckingsAmount() {
-      return 0;
+      return checkingsAmount;
    }
 
    unsigned long Account::getSavingsAmount() {
-      return 0;
+      return savingsAmount;
    }
 
    string Account::getName() {
-      return string("");
+      return name;
    }
 }
