8e9aa55e55187797d47207ffc482cf4d57f10b39
Emil Meng <emil.meng@gmail.com>
2011-01-16 23:16:21 -0700

    whoops, forgot to add this file

---
+++ DataStructures/HW03/testll.cxx
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include "LinkedList.h"
+
+using namespace net_oatsnet_class_datastructures;
+
+class LinkedListTest : public testing::Test {
+   protected: 
+      virtual void SetUp() {
+      }
+
+      virtual void TearDown() {
+      }
+
+      // Declares the variables your tests want to use.
+      LinkedList myll0;
+      LinkedList myll1;
+      LinkedList myll2;
+};
